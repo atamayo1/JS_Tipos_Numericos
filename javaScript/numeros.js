19f26c7ae8a1c4959a9c474f76a6caeb75897384
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2017-07-08 11:08:31 -0500

    Manejo de alerts con números terminado

---
+++ javaScript/numeros.js
@@ -0,0 +1,20 @@
+//manejo de numero es JS
+//Declaracion de una variable
+//Su valor inicial es undefined
+var numero;
+//Se le asigna el valor y se le da el tipo numerico
+numero=100;
+numero= -10000;
+
+//Valor octal
+var octal;
+octal=03267;
+alert(octal);
+//valores hexadecimal
+var hexadecimal;
+hexadecimal = 0xff;
+alert(hexadecimal);
+
+//Valor fraccional
+numero = 3.1416;
+alet(numero);
